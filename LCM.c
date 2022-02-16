e6beccf627521fab01701b7161bf105cc1a670a0
rajeshmaradani <96291809+rajeshmaradani@users.noreply.github.com>
2022-02-16 21:17:40 +0530

    #include<stdio.h> int main() {     int m,n,lcm,i; 	scanf("%d%d",&m,&n); 	int max=m<n?n:m; 	for(i=max;i>=max;i++) 	{ 		if(i%m==0 && i%n==0) 		{ 			lcm=i; 			break; 		}     }     printf("%d",lcm);     return 0; }

---
+++ LCM.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,lcm,i;
+	scanf("%d%d",&m,&n);
+	int max=m<n?n:m;
+	for(i=max;i>=max;i++)
+	{
+		if(i%m==0 && i%n==0)
+		{
+			lcm=i;
+			break;
+		}
+    } 	
+    printf("%d",lcm);
+    return 0;
+}
